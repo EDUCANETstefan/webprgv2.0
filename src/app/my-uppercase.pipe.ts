8fe41cd010046c0dc8ffc940efb4e29ffb3a694e
EDUCANETstefan <stefan.rajilic@educanet.cz>
2021-01-11 11:05:17 +0100

    pipe opravena form validace pridana

--- src/app/my-uppercase.pipe.ts
+++ src/app/my-uppercase.pipe.ts
@@ -5,11 +5,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class MyUppercasePipe implements PipeTransform {
 
-  transform(value: string): string {
-    const dots = "...";
-    const limit = 3;
-    if(value.length > limit) return value.substring(0, limit) + dots;
-      else return value;
+  transform(value: string, limit: number, dots: string): string {
+
+    const fullLenght = value.length + dots.length;
+
+    if(value.length < limit) return value;
+      else if(dots.length >= limit) return dots.substring(0, limit);
+        else if(fullLenght > limit) return value.substring(0, limit - dots.length) + dots;
   }
 
 }
